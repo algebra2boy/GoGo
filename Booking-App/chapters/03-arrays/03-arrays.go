5760d0474d8c0da66823e1d730b701027e0eeb73
Yongye Tan <yongyetan1209@gmail.com>
2024-03-06 01:11:06 -0500

    add variable and array

--- Booking-App/chapters/03-arrays/03-arrays.go
+++ Booking-App/chapters/03-arrays/03-arrays.go
@@ -8,6 +8,10 @@ func main() {
 	var officeHourStart int = 9
 	officeHourSession := 8
 
+	// bookings := []string{"Apple", "Banana", 4} // This will not compile since it contains different types
+	// bookings := [20]string{} // 20 is the length of the array
+	bookings := []string{} // slice, dyanmic array with no limit
+
 	fmt.Printf("Welcome to %v at %vam for %v session \n", officeHourName, officeHourStart, officeHourSession)
 
 	var firstname, lastname, email, userName string
@@ -23,6 +27,12 @@ func main() {
 
 	fmt.Println("Enter your username: ")
 	fmt.Scanln(&userName)
+	bookings = append(bookings, firstname+" "+lastname)
+
+	fmt.Printf("Current bookings: %v\n", bookings)
+	fmt.Printf("first booking: %v\n", bookings[0])
+	fmt.Printf("Array type %T\n", bookings) // []string
+	fmt.Printf("Array length %v\n", len(bookings))
 
 	fmt.Printf("Welcome %v %v, your email is %v and your username is %v \n", firstname, lastname, email, userName)
 }
