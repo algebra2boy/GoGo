99974e4f3744bda499039907146a84cec8377106
Yongye Tan <yongyetan1209@gmail.com>
2024-03-29 19:42:46 -0400

    finish the scraping tutorial

--- web-scraper-go/README.md
+++ web-scraper-go/README.md
@@ -1,3 +1,6 @@
 # web-scraper-go
 
-Learning how to web scrape in golang using Colly package. This is going to be my first web scraper project! 
+Learning how to web scrape in golang using Colly package. This is going to be my first web scraper project! I am learning it from the following website.
+
+# Reference
+[Put All Together: Final Code](https://www.zenrows.com/blog/web-scraping-golang#final-code)
--- web-scraper-go/scraper.go
+++ web-scraper-go/scraper.go
@@ -8,66 +8,113 @@ import (
 	"github.com/gocolly/colly"
 )
 
-/*
-OnRequest(): Called before performing an HTTP request with Visit().
-OnError(): Called if an error occurred during the HTTP request.
-OnResponse(): Called after receiving a response from the server.
-OnHTML(): Called right after OnResponse() if the received content is HTML.
-OnScraped(): Called after all OnHTML() callback executions.
-*/
-
+// defining a data structure to store the scraped data
 type PokemonProduct struct {
 	url, image, name, price string
 }
 
-func main() {
+// it verifies if a string is present in a slice
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
 
+	return false
+}
+
+func main() {
+	// initializing the slice of structs that will contain the scraped data
 	var pokemonProducts []PokemonProduct
 
+	// initializing the list of pages to scrape with an empty slice
+	var pagesToScrape []string
+
+	// the first pagination URL to scrape
+	pageToScrape := "https://scrapeme.live/shop/page/1/"
+
+	// initializing the list of pages discovered with a pageToScrape
+	pagesDiscovered := []string{pageToScrape}
+
+	// current iteration
+	i := 1
+	// max pages to scrape
+	limit := 5
+
+	// initializing a Colly instance
 	c := colly.NewCollector()
+	// setting a valid User-Agent header
+	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 
-	//  Note that the e parameter of the callback function represents a single li.product HTMLElement
-	// target li HTML element has the .product class
+	// iterating over the list of pagination links to implement the crawling logic
+	c.OnHTML("a.page-numbers", func(e *colly.HTMLElement) {
+		// discovering a new page
+		newPaginationLink := e.Attr("href")
+
+		// if the page discovered is new
+		if !contains(pagesToScrape, newPaginationLink) {
+			// if the page discovered should be scraped
+			if !contains(pagesDiscovered, newPaginationLink) {
+				pagesToScrape = append(pagesToScrape, newPaginationLink)
+			}
+			pagesDiscovered = append(pagesDiscovered, newPaginationLink)
+		}
+	})
+
+	// scraping the product data
 	c.OnHTML("li.product", func(e *colly.HTMLElement) {
-
 		pokemonProduct := PokemonProduct{}
 
-		// get the href attribute of the a element
 		pokemonProduct.url = e.ChildAttr("a", "href")
-
-		// get the src attribute of the img element
 		pokemonProduct.image = e.ChildAttr("img", "src")
-
-		// get the text of the h2 element
 		pokemonProduct.name = e.ChildText("h2")
-
-		// get the text with the class .price
 		pokemonProduct.price = e.ChildText(".price")
 
 		pokemonProducts = append(pokemonProducts, pokemonProduct)
-
 	})
 
-	c.Visit("https://scrapeme.live/shop/")
+	c.OnScraped(func(response *colly.Response) {
+		// until there is still a page to scrape
+		if len(pagesToScrape) != 0 && i < limit {
+			// getting the current page to scrape and removing it from the list
+			pageToScrape = pagesToScrape[0]
+			pagesToScrape = pagesToScrape[1:]
 
-	// opening the csv file
-	file, err := os.Create("pokemon.csv")
+			// incrementing the iteration counter
+			i++
 
+			// visiting a new page
+			c.Visit(pageToScrape)
+		}
+	})
+
+	// visiting the first page
+	c.Visit(pageToScrape)
+
+	// opening the CSV file
+	file, err := os.Create("products.csv")
 	if err != nil {
-		log.Fatalln("Failed to open file", err)
+		log.Fatalln("Failed to create output CSV file", err)
 	}
-	defer file.Close() // Close closes the File, rendering it unusable for I/O
+	defer file.Close()
 
-	// initializing a file writter
+	// initializing a file writer
 	writer := csv.NewWriter(file)
 
 	// defining the CSV headers
-	headers := []string{"url", "image", "name", "price"}
-
-	// writting the colume headers
+	headers := []string{
+		"url",
+		"image",
+		"name",
+		"price",
+	}
+	// writing the column headers
 	writer.Write(headers)
 
+	// adding each Pokemon product to the CSV output file
 	for _, pokemonProduct := range pokemonProducts {
+		// converting a PokemonProduct to an array of strings
 		record := []string{
 			pokemonProduct.url,
 			pokemonProduct.image,
@@ -75,8 +122,8 @@ func main() {
 			pokemonProduct.price,
 		}
 
+		// writing a new CSV record
 		writer.Write(record)
 	}
-	defer writer.Flush() // Flush writes any buffered data to the underlying io.Writer.
-
+	defer writer.Flush()
 }
